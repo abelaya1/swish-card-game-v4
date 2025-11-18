97081b309da6a4e3ba1279c5c4a4a825ea8d0b66
Abel Ayala <abelaya1@nmsu.edu>
2025-11-18 14:52:10 -0700

    Updated UI & fixed bugs

--- swish-card-game-main/server/index.js
+++ swish-card-game-main/server/index.js
@@ -285,7 +285,8 @@ if (S.activePlayer !== 'AI' && S.activePlayer !== socket.id) {
   });
 }); // closes io.on('connection')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
